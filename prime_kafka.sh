f6fb36df0f5a314eecbc57cdf4151ee7591c25c7
Mike Lubinets <lubinetsm@yandex.ru>
2018-09-27 10:49:18 +0300

    enable log compaction

--- prime_kafka.sh
+++ prime_kafka.sh
@@ -11,14 +11,25 @@ function create_topic() {
 	    --replication-factor 1
 }
 
+function enable_compaction() {
+   echo "Enabling log compaction on topic $1"
+   kafka-topics.sh \
+        --zookeeper $ZOOKEEPER \
+        --alter --topic $1 \
+        --config cleanup.policy=compact
+}
+
 # Testing topics
 create_topic "rustyrobot.test.state.save_and_restore" 1
 create_topic "rustyrobot.test.handler.in" 1
 create_topic "rustyrobot.test.handler.out" 1
+enable_compaction "rustyrobot.test.state.save_and_restore"
 
 
 # Github microservice input & output
 create_topic "rustyrobot.github.request" 1
 create_topic "rustyrobot.github.event" 1
+create_topic "rustyrobot.github.state" 1
+enable_compaction "rustyrobot.github.state"
 
 
